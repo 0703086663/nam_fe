9ae51fecf6f08d0fbd5138ed530feaada9392202
Nguyễn Tiến Thành <nguyentienthanh.tgdd@gmail.com>
2024-03-16 18:35:31 +0700

    Save

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { useState } from "React";
+// import { useState } from "React";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
@@ -9,21 +9,21 @@ import Survey from "./pages/Survey";
 import "./index.css";
 
 export default function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
+  // const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <BrowserRouter>
       <Routes>
         {/* {!loggedIn && <Route path="/" element={<Login />} />} */}
 
-        {loggedIn && (
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Campaign />} />
-            <Route path="field" element={<Field />} />
-            <Route path="response" element={<Response />} />
-            <Route path="survey" element={<Survey />} />
-          </Route>
-        )}
+        {/* {loggedIn && ( */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Campaign />} />
+          <Route path="field" element={<Field />} />
+          <Route path="response" element={<Response />} />
+          <Route path="survey" element={<Survey />} />
+        </Route>
+        {/* )} */}
       </Routes>
     </BrowserRouter>
   );
